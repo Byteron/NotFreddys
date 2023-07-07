5baec64fe9d48c34f310a3a00c5c4ba17deaeae5
Aaron Winter <winter.aaron93@gmail.com>
2023-07-07 20:26:38 +0200

    initial movement code and project settings

--- src/level/level.gd
+++ src/level/level.gd
@@ -1,3 +1,33 @@
 extends Node2D
 class_name Level
 
+const GRID_SIZE := Vector2(16, 16)
+
+@onready var monster: Node2D = $Monster
+
+
+func _ready() -> void:
+	monster.cell = (monster.position / GRID_SIZE).floor()
+
+
+func _process(delta: float) -> void:
+	var direction := get_input_direction()
+	move_monster(direction)
+
+
+func get_input_direction() -> Vector2:
+	var x := int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	var y := int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
+	return Vector2(x, y).normalized()
+
+
+func move_monster(direction: Vector2) -> void:
+	if not direction or monster.is_moving:
+		return
+	
+	monster.cell = monster.cell + direction
+	monster.is_moving = true
+	
+	var tween := get_tree().create_tween()
+	tween.tween_property(monster, "position", monster.cell * GRID_SIZE, 0.15)
+	tween.tween_callback(func(): monster.is_moving = false)
---
+++ src/monster/monster.gd
@@ -0,0 +1,5 @@
+extends Node2D
+class_name Monster
+
+var cell: Vector2
+var is_moving: bool
